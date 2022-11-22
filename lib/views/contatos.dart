7058354b3bc78d51e8da946c1df62e1b9ca4f8b5
Peedro henriquee <vick.costa1305@gmail.com>
2022-11-22 19:37:55 -0300

    atualizacao fire

--- lib/views/contatos.dart
+++ lib/views/contatos.dart
@@ -1,6 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:app/views/dados.dart';
 
 class Contatos extends StatelessWidget {
+
+ final controller = TextEditingController();
+  final controllerUrl = TextEditingController();
+   final controllerName = TextEditingController();
+    final controllerTelefone = TextEditingController();
+     final controllerEmail = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,8 +27,15 @@ class Contatos extends StatelessWidget {
           IconButton(
               icon: Icon(Icons.save),
               onPressed: () {
-                Navigator.of(context).pop();
-              })
+                final user = User(name: controllerName.text,telefone: int.parse(controllerTelefone.text),
+                email: controllerEmail.text,
+                url: controllerUrl.text);
+
+                createUser(user, email: '', url: '', name: '');
+
+               Navigator.of(context).pushNamed('/dados');
+              
+              }),
         ],
       ),
       body: Padding(
@@ -22,20 +44,88 @@ class Contatos extends StatelessWidget {
           child: Column(
             children: <Widget>[
               TextFormField(
+                controller: controllerName,
                 decoration: InputDecoration(labelText: 'Nome:'),
               ),
               TextFormField(
+                controller: controllerTelefone,
                 decoration: InputDecoration(labelText: 'Telefone:'),
               ),
               TextFormField(
+                controller: controllerEmail,
                 decoration: InputDecoration(labelText: 'E-mail:'),
               ),
               TextFormField(
+                controller: controllerUrl,
                 decoration: InputDecoration(labelText: 'URL do Avatar:'),
               ),
             ],),)
         ),
       
     );
+    
   }
+
+
+  Future createUser(User user, {required String name, required String email, required String url}) async {
+    final docUser = FirebaseFirestore.instance.collection('u').doc();
+
+    final user= User(
+      id: docUser.id,
+      name : name,
+      telefone : 21,
+      email : email,
+      url : url,
+    );
+
+    final json = user.toJson();
+    
+    await docUser.set(json);
+  }
+}
+
+class User {
+  String id;
+  late final String name;
+  int telefone;
+  late final String email;
+  late final String url;
+
+  User({
+    this.id= '',
+    required this.name,
+    required this.telefone,
+    required this.email,
+     required  this.url,
+  });
+  
+  
+  
+
+Map<String,dynamic> toJson() => {
+    
+    'id': id,
+    'name' : name,
+    'telefone' : telefone,
+    'email': email,
+    'url' : url,
+  };
+
+
+static User fromJson(Map<String, dynamic> json) => User(
+    id: json['id'],
+    name: json['name'],
+    telefone: json['telefone'],
+    email: json['email'],
+    url : json['url'],
+  );
+
+
+Future createUser(User user) async{
+  final docUser = FirebaseFirestore.instance.collection('u').doc();
+  user.id = docUser.id;
+
+  final json= user.toJson();
+  await docUser.set(json);
+}
 }
--- lib/views/dados.dart
+++ lib/views/dados.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:app/views/contatos.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -6,53 +8,101 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/cupertino.dart';
 
 class Dados extends StatelessWidget {
- 
-
-  final controller = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: TextField(controller: controller),actions: [IconButton(icon: Icon(Icons.add),
-    onPressed:() {
-      Navigator.of(context).pushNamed('/ct');
-      final name = controller.text;
-
-      createUser(name: name);
-    }),],),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TELA DE CONTATOS'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/ct');
+              }),
+        ],
+      ),
+      body: StreamBuilder<List<User>>(
+          stream: readUsers(), builder: (context, snapshot){if (snapshot.hasError) {
+            return Text('ERrooooo ${snapshot}');}else if(snapshot.hasData){
+              final users = snapshot.data!;
+            
+            return ListView(
+              children: users.map(buildUser).toList(), );
+            
+            }else{return Center(child: CircularProgressIndicator());
+            }
+  }),
+    );
   }
-Future createUser({required String name}) async {
-    final docUser = FirebaseFirestore.instance.collection('users').doc();
 
-    const user = User;{
-      id: docUser.id;
-      name : name;
-      idade : 21;
-      aniversario : DateTime(2001, 8, 15);
-    };
+ 
+}
 
-    final json = user.toJson();
-    
-    await docUser.set(json);
-  }
+Future createUser(User user,
+    {required String name, required String email, required String url}) async {
+  final docUser = FirebaseFirestore.instance.collection('u').doc();
+
+  final user = User(
+    id: docUser.id,
+    name: name,
+    telefone: 21,
+    email: email,
+    url: url,
+  );
+
+  final json = user.toJson();
+
+  await docUser.set(json);
 }
 
 class User {
   String id;
-  final String name;
-  final int idade;
-  final DateTime aniversario;
+  late final String name;
+  int telefone;
+  late final String email;
+  late final String url;
 
   User({
-    this.id= '',
+    this.id = '',
     required this.name,
-    required this.idade,
-    required this.aniversario,
+    required this.telefone,
+    required this.email,
+    required this.url,
   });
 
   Map<String, dynamic> toJson() => {
-    'id': 'id',
-    'name' : name,
-    'age' : idade,
-    'aniversario': aniversario,
-  };
+        'id': id,
+        'name': name,
+        'telefone': telefone,
+        'email': email,
+        'url': url,
+      };
+
+  static User fromJson(Map<String, dynamic> json) => User(
+        id: json['id'],
+        name: json['name'],
+        telefone: json['telefone'],
+        email: json['email'],
+        url: json['url'],
+      );
+
+  Future createUser(User user) async {
+    final docUser = FirebaseFirestore.instance.collection('u').doc();
+    user.id = docUser.id;
+
+    final json = user.toJson();
+    await docUser.set(json);
+  }
+  
 }
+Widget buildUser(User user) => ListTile(
+  leading: CircleAvatar(child: Image.asset('images/R.jpg')),
+  title: Text((user.name)),
+  
+  tileColor: Colors.green[100],
+);
+
+Stream<List<User>> readUsers() =>
+      FirebaseFirestore.instance.collection('u').snapshots().map((snapshot) =>
+          snapshot.docs.map((doc) => User.fromJson(doc.data())).toList());
